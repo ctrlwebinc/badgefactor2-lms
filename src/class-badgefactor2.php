7b82093ed94e7374f7a89245f674a133e3e7ce58
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2020-06-04 08:49:39 -0400

    added template stubs for assertion and badge

--- src/class-badgefactor2.php
+++ src/class-badgefactor2.php
@@ -112,6 +112,8 @@ class BadgeFactor2 {
 		require_once BF2_ABSPATH . 'src/core/class-badgruser.php';
 		require_once BF2_ABSPATH . 'src/client/shortcodes/class-issuers.php';
 		require_once BF2_ABSPATH . 'src/core/class-badgrindividualclient.php';
+		require_once BF2_ABSPATH . 'src/public/class-badgefactor2-public.php';
+		BadgeFactor2_Public::init_hooks();
 
 		add_action( 'init', array( BadgrProvider::class, 'init_hooks' ) );
 		add_action( 'init', array( BadgrUser::class, 'init_hooks' ) );
