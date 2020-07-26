88c2923e1595a3f882e0137815969ea6f27d7a02
Miguel Legault <miguel.legault@ctrlweb.ca>
2020-07-26 12:11:18 -0400

    Add issuers count Adapt badge classes count to badgr bf2 url scheme

--- src/core/class-badgrprovider.php
+++ src/core/class-badgrprovider.php
@@ -255,6 +255,24 @@ class BadgrProvider {
 		return false;
 	}
 
+
+	public static function get_all_issuers_count( ) {
+		// Make GET request to /v2/issuers.
+		$response = self::get_client()->get( '/v2/issuers_count' );
+
+		// Check for 200 response.
+		if ( null !== $response && 200 === $response->getStatusCode() ) {
+			$response_info = json_decode( $response->getBody() );
+			if ( isset( $response_info->status->success ) &&
+				true == $response_info->status->success &&
+				isset( $response_info->count ) && is_numeric( $response_info->count ) ) {
+					return intval( $response_info->count );
+			}
+		}
+
+		return false;
+	}
+
 	/**
 	 * Get Badgr Issuer by entity ID / slug.
 	 *
@@ -511,7 +529,7 @@ class BadgrProvider {
 	 * @return mixed
 	 */
 	public static function get_all_badge_classes_count() {
-		$response = self::get_client()->get( '/v2/badgeclasses', array( 'limit' => 1, 'offset' => 0 ) );
+		$response = self::get_client()->get( '/v2/badgeclasses_count' );
 
 		// Check for 200 response.
 		if ( null !== $response && 200 === $response->getStatusCode() ) {
@@ -715,7 +733,7 @@ class BadgrProvider {
 	 * @return mixed
 	 */
 	public static function get_all_assertions_by_badge_class_slug_count( $badge_class_slug ) {
-		$response = self::get_client()->get( '/v2/badgeclasses/' . $badge_class_slug . '/assertions', array( 'limit' => 1, 'offset' => 0 ) );
+		$response = self::get_client()->get( '/v2/badgeinstances_count/badgeclass' . $badge_class_slug );
 
 		// Check for 200 response.
 		if ( null !== $response && 200 === $response->getStatusCode() ) {
@@ -771,9 +789,8 @@ class BadgrProvider {
 	}
 
 	public static function get_all_assertions_by_issuer_slug_count( $issuer_slug)  {
-		// Make GET request to /v2/issuers/{entity_id}/assertions.
 		$response = self::get_client()->get(
-			'/v2/issuers/' . $issuer_slug . '/assertions', array( 'limit' => 1, 'offset' => 0 ) );
+			'/v2/badgeinstances_count/issuer/' . $issuer_slug );
 
 		// Check for 200 response.
 		if ( null !== $response && 200 === $response->getStatusCode() ) {
