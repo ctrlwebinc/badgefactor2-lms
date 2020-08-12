695cd95dd280ad8457580026dc1cc46408db15c8
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2020-08-12 08:33:24 -0400

    added FIXME comment

--- assets/js/public.js
+++ assets/js/public.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
   $(document).on("submit", ".badge-request-form", function(e) {
     e.preventDefault();
     $.post(ajaxurl, $(this).serialize(), function(response) {
+      // FIXME Do something with the response.
       console.log("The server responded: ", response);
     });
   });
