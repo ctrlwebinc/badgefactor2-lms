0a79ff15d9464c6cdde06a1318d825a4ec02702c
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2020-10-01 12:10:28 -0400

    style refactoring to WordPress coding style

--- templates/tpl.badge-pages.php
+++ templates/tpl.badge-pages.php
@@ -30,9 +30,9 @@
 
 get_header();
 
-$customPostType 	  = 'badge-page';
-$customTaxonomy 	  = 'badge-category';
-$termsByBadgeCategory = get_terms($customTaxonomy);
+$custom_post_type        = 'badge-page';
+$custom_taxonomy         = 'badge-category';
+$terms_by_badge_category = get_terms( $custom_taxonomy );
 
 ?>
 
@@ -45,56 +45,58 @@ $termsByBadgeCategory = get_terms($customTaxonomy);
 
 		<div class="c-bf2__body">
 			<?php
-			foreach($termsByBadgeCategory as $custom_term) {
+			foreach ( $terms_by_badge_category as $custom_term ) {
 
 				wp_reset_query();
 
-				$args = array('post_type' => $customPostType,
+				$args = array(
+					'post_type' => $custom_post_type,
 					'tax_query' => array(
 						array(
-							'taxonomy' => $customTaxonomy,
+							'taxonomy' => $custom_taxonomy,
 							'field'    => 'slug',
-							'terms'    => $custom_term->slug
-						)
-					)
+							'terms'    => $custom_term->slug,
+						),
+					),
 				);
 
-				$loop = new WP_Query($args);
-				
-				if($loop->have_posts()) { ?>
+				$loop = new WP_Query( $args );
+
+				if ( $loop->have_posts() ) {
+					?>
 					<section class="c-bf2__section">
 						<h2 class="c-bf2__section__title"><?php echo $custom_term->name; ?></h2>
 						<p class="c-bf2__section__description">
-							<?php echo $custom_term->description;?>
+							<?php echo $custom_term->description; ?>
 						</p>
 						<div class="c-bf2__list__items">
 							<?php
-							while($loop->have_posts()) : $loop->the_post();
-								
+							while ( $loop->have_posts() ) :
+								$loop->the_post();
+
 								$badge_entity_id = get_post_meta( $post->ID, 'badge', true );
 								$badge           = BadgeFactor2\Models\BadgeClass::get( $badge_entity_id );
-								$issuer          = BadgeFactor2\Models\Issuer::get( $badge->issuer ); ?>
+								$issuer          = BadgeFactor2\Models\Issuer::get( $badge->issuer );
+								?>
 
 								<div class="c-bf2__badge c-bf2__list__item">
-									<a class="c-bf2__badge__inner" href="<?php echo get_permalink($post->ID); ?>">
+									<a class="c-bf2__badge__inner" href="<?php echo get_permalink( $post->ID ); ?>">
 										<img class="c-bf2__badge__image" src="<?php echo $badge->image; ?>" alt="<?php echo $badge->name; ?>">
 										<h3 class="c-bf2__badge__title"><?php echo $badge->name; ?></h3>
 									</a>
 								</div>
-								
-							<?php
+								<?php
 							endwhile;
 							?>
 						</div>
 					</section>
-				<?php
+					<?php
 				}
 				?>
-			<?php
+				<?php
 			}
 			?>
 		</div>
-
 	</div>
 </main>
 <?php
