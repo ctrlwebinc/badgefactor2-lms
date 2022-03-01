5261fedfeb97d4e3d0d42b4eaa92dfb81fb919f8
Miguel Legault -- ctrlweb <miguel.legault@ctrlweb.ca>
2022-03-01 15:05:06 -0500

    Remove logging from js

--- assets/js/privacy.js
+++ assets/js/privacy.js
@@ -18,8 +18,6 @@
  */
 jQuery(document).ready(function ($) {
 
-    console.log('Privacy js loaded');
-
     $('#assertion_visibility_toggle').on('click', function (e) {
 
         $('#assertion_visibility_toggle').addClass('visibility-updating');
@@ -35,7 +33,6 @@ jQuery(document).ready(function ($) {
             },
             success: function (response) {
                 $('#assertion_visibility_toggle').removeClass('visibility-updating');
-                 console.log(response);
                  if ( response.has_privacy_flag == true ) {
                     $('#assertion_visibility_toggle').addClass('visibility-private');
                  } else {
@@ -46,16 +43,4 @@ jQuery(document).ready(function ($) {
         });
     })
 
-/*     jQuery.ajax({
-        type : "post",
-        dataType : "json",
-        url : bf2_privacy_ajax.callback_parameters.ajax_endpoint,
-        data : {
-            action: bf2_privacy_ajax.callback_parameters.ajax_action,
-            badge_slug: bf2_privacy_ajax.callback_parameters.badge_slug,
-            nonce: bf2_privacy_ajax.callback_parameters.nonce
-        },
-        success: function (response) { console.log(response);},
-        error: function ( error ) { console.log(error);}
-    }); */
 });
