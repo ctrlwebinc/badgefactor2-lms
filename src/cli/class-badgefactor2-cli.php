acbe54ec592b4dac5ea016e60d2d8296cba4c5d6
Tahina <to.tahina@gmail.com>
2022-08-22 15:00:03 +0300

    CLI for updating badge request content meta

--- src/cli/class-badgefactor2-cli.php
+++ src/cli/class-badgefactor2-cli.php
@@ -242,4 +242,61 @@ class BadgeFactor2_CLI extends WP_CLI_Command {
 
 		WP_CLI::success( json_encode(AssertionPrivacy::generate_ajax_callback_parameters( $args[0] )));
 	}
+
+	public function make_badge_request_forms_require_login( $args, $assoc_args ) {
+		if ( ! class_exists( 'GFCommon' ) ) {
+			WP_CLI::error( sprintf( 'Gravity Forms is not active!' ) );
+		}
+
+		$badge_pages = BadgePage::all();
+		$count = count( $badge_pages );
+		$progress = WP_CLI\Utils\make_progress_bar( sprintf( '%d badge pages to verify.', $count ), $count );
+		$fixed = 0;
+		foreach ( BadgePage::all() as $badge_page ) {
+			$metas = get_post_meta( $badge_page->ID );
+
+			if ( 'gravityforms' === $metas['badge_request_form_type'][0] && isset( $metas['badge_request_form_id'][0] ) ) {
+				$gravityform = \GFAPI::get_form( $metas['badge_request_form_id'][0] );
+				if ( $gravityform && ( ! isset( $gravityform['requireLogin'] ) || false === $gravityform['requireLogin'] ) ) {
+					$gravityform['requireLogin'] = true;
+					$result = \GFAPI::update_form( $gravityform );
+					if ( ! $result ) {
+						WP_CLI::error( sprintf( 'An unknown error has occured.' ) );
+					}
+					$fixed++;
+				}
+			}
+			$progress->tick();
+		}
+		WP_CLI::log( sprintf( '%d badge request forms fixed!', $fixed ) );
+
+	}
+
+	public function update_badge_requests_meta_content ( $args, $assoc_args ) {
+		global $wpdb;
+		$results = $wpdb->get_results( "
+			SELECT p.ID, post_content, meta_value 
+			FROM {$wpdb->posts} p
+			JOIN {$wpdb->postmeta} pm
+			ON p.ID = pm.post_id
+			WHERE meta_key = 'content'
+			AND meta_value = 'Formulaire soumis'", 
+			OBJECT 
+		);
+
+		foreach( $results as $post ) {
+			$wpdb->update( 
+				$wpdb->postmeta, 
+				array( 
+					'meta_value' => $post->post_content 
+				), 
+				array( 'post_id' => $post->ID ), 
+				array( 
+					'%s',   // value1
+				), 
+				array( '%d' ) 
+			);
+			echo 'Updating post ID : ' . $post->ID . PHP_EOL;
+		}
+	}
 }
