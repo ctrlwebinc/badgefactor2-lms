15075dc956cfc94832b8195151b292ff19f4fe7a
Miguel Legault -- ctrlweb <miguel.legault@ctrlweb.ca>
2023-01-17 16:52:56 -0500

    Fix dates labels

--- src/admin/cmb2-fields/class-dates.php
+++ src/admin/cmb2-fields/class-dates.php
@@ -60,7 +60,7 @@ class Dates {
 			'revision'  => __( 'Revision requested', BF2_DATA['TextDomain'] ),
 			'revoked'   => __( 'Revoked', BF2_DATA['TextDomain'] ),
 			'revision_cancelled'   => __( 'Revision cancelled', BF2_DATA['TextDomain'] ),
-			'reject_cancelled'   => __( 'Rejection cancelled', BF2_DATA['TextDomain'] ),
+			'rejection_cancelled'   => __( 'Rejection cancelled', BF2_DATA['TextDomain'] ),
 		);
 
 		foreach ( $dates as $data ) {
