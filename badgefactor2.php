f5d58d16a777b6f37d77021ced46e31c0b264c97
Miguel Legault -- ctrlweb <miguel.legault@ctrlweb.ca>
2023-04-24 18:30:03 -0400

    Use correct version. Non-breaking, keep major number

--- badgefactor2.php
+++ badgefactor2.php
@@ -24,7 +24,7 @@
  * GitHub Plugin URI: https://github.com/ctrlwebinc/badgefactor2
  * Description: Issues and manages Open Badges with Badgr server
  * Author: ctrlweb
- * Version: 2.2.0
+ * Version: 1.12.0
  * Author URI: https://badgefactor2.com/
  * License: GNU AGPL
  * Text Domain: badgefactor2
